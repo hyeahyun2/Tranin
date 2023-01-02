5068bc40982bee29d77844ce453242402314eae8
yusuhyeon <yusuhyun0716@gmail.com>
2023-01-02 15:34:22 +0900

    로그인 구현

---
+++ src/main/java/controller/loginFormServlet.java
@@ -0,0 +1,37 @@
+package controller;
+
+import dao.MemberSelectLoginDao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.rmi.ServerException;
+import java.rmi.server.ServerCloneException;
+
+@WebServlet("/loginFormServlet")
+public class loginFormServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException{
+        request.setCharacterEncoding("utf-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        String memberId = request.getParameter("memberId");
+        String password = request.getParameter("password");
+
+        MemberSelectLoginDao dao = new MemberSelectLoginDao();
+
+        boolean state = dao.login(memberId,password);
+
+
+        if(state){ // 회원가입 성공
+            request.getSession().setAttribute("memberId", memberId);
+            response.sendRedirect("index.jsp");
+        }
+        else { // 회원가입 실패
+            response.sendRedirect("register.jsp");
+        }
+    }
+
+}
---
+++ src/main/java/dao/MemberSelectLoginDao.java
@@ -0,0 +1,39 @@
+package dao;
+
+public class MemberSelectLoginDao {
+
+    public boolean login(String id, String pw) {
+
+        DBProperty db = new DBProperty();
+        String sql = "SELECT id, pw from tranin_member where id=? and pw=?";
+        int upd = 0;
+        try {
+            db.pstmt = db.conn.prepareStatement(sql);
+            db.pstmt.setString(1, id);
+            db.pstmt.setString(2, pw);
+
+            db.rs=db.pstmt.executeQuery();
+            while(db.rs.next()){
+                if(id.equals(db.rs.getString("id"))){
+                    System.out.println("로그인 정보 일치");
+                    return true;
+                }else{
+                    System.out.println("로그인 정보 불일치");
+                    return false;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (db.pstmt != null) db.pstmt.close();
+                if (db.conn != null) db.conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+}
