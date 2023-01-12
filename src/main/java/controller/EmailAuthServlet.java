9f4a31b4ee4e4ae3f123b1732884b62581ed9289
yusuhyeon <yusuhyun0716@gmail.com>
2023-01-12 09:40:02 +0900

    ㅠㅠ

--- src/main/java/controller/EmailAuthServlet.java
+++ src/main/java/controller/EmailAuthServlet.java
@@ -30,15 +30,12 @@ public class EmailAuthServlet extends HttpServlet {
         System.out.println(email);
 
 
-        //mail server 설정
-        String host = "smtp.naver.com";
-        String user = ""; //자신의 네이버 계정
-        String password = " ";//자신의 네이버 패스워드
+        String host = "smtp.googlemail.com";
+        String usr = "yusuhyun0716@gmail.com";
+        String pss = "dthzisevvfoiswst";
 
-        //메일 받을 주소
         String to_email = email;
 
-        //SMTP 서버 정보를 설정한다.
         Properties props = new Properties();
         props.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.smtp.host", host);
@@ -47,22 +44,18 @@ public class EmailAuthServlet extends HttpServlet {
         props.put("mail.smtp.ssl.enable", "true");
 
 
-        //인증 번호 생성기
         StringBuffer temp = new StringBuffer();
         Random rnd = new Random();
         for (int i = 0; i < 10; i++) {
             int rIndex = rnd.nextInt(3);
             switch (rIndex) {
                 case 0:
-                    // a-z
                     temp.append((char) ((int) (rnd.nextInt(26)) + 97));
                     break;
                 case 1:
-                    // A-Z
                     temp.append((char) ((int) (rnd.nextInt(26)) + 65));
                     break;
                 case 2:
-                    // 0-9
                     temp.append((rnd.nextInt(10)));
                     break;
             }
@@ -72,30 +65,26 @@ public class EmailAuthServlet extends HttpServlet {
 
         Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
             protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-                return new javax.mail.PasswordAuthentication(user, password);
+                return new javax.mail.PasswordAuthentication(usr, pss);
             }
         });
 
-        //email 전송
         try {
             MimeMessage msg = new MimeMessage(session);
-            msg.setFrom(new InternetAddress(user, "Tranin"));
+            msg.setFrom(new InternetAddress(usr, "Tranin"));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to_email));
 
-            //메일 제목
             msg.setSubject("안녕하세요 Tranin 인증 메일입니다.");
-            //메일 내용
-            msg.setText("인증 번호는 :" + temp);
+            msg.setText("인증 번호는 :" + temp + "입니다.");
 
             Transport.send(msg);
             System.out.println("이메일 전송");
 
         } catch (Exception e) {
-            e.printStackTrace();// TODO: handle exception
+            e.printStackTrace();
         }
         HttpSession saveKey = request.getSession();
         saveKey.setAttribute("AuthenticationKey", AuthenticationKey);
-        //패스워드 바꿀때 뭘 바꿀지 조건에 들어가는 id
     }
 
 }
