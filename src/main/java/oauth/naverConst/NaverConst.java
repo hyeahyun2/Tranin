86cf41c414d3586be7f8e6e05a7010c0ecd39212
JJoon <chipp0604@gmail.com>
2023-03-24 11:54:47 +0900

    네이버 로그인 수정

--- src/main/java/oauth/naverConst/NaverConst.java
+++ src/main/java/oauth/naverConst/NaverConst.java
@@ -4,7 +4,8 @@ public class NaverConst {
 	
 	static final public String clientId = "BZKQ5WgWUgC77id_pWvV";
 	
-	static final public String redirectURL = "http://localhost:8080/oauth/naver/token";
+//	static final public String redirectURL = "http://localhost:8080/oauth/naver/token";
+	static final public String redirectURL = "http://runeah.cafe24.com/oauth/naver/token";
 	
 	static final public String clientSecret = "7lWwWeb09z";
 	
